634480c13b1cf90fbdd8bc6d8956daccb4fcde81
Khaue-Souza <81392244+Khaue-Souza@users.noreply.github.com>
2023-09-15 21:40:10 -0300

    correção

---
+++ Backend/Clinivet1/Models/APIdbcontext.cs
@@ -0,0 +1,20 @@
+﻿using MangasAPI.Models;
+using NovelsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using UsuarioAPI.Models;
+
+namespace MangaNovelsAPI.Models
+{
+    public class APIdbcontext : DbContext
+    {
+        public APIdbcontext(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        public DbSet<Mangas> mangas { get; set; }
+        public DbSet<Novels> novels { get; set; }
+        public DbSet<Usuario> usuarios { get; set; }
+
+    }
+}
---
+++ Backend/Clinivet1/Models/Mangas.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace MangasAPI.Models
+{
+    //Criação da tabela e campos
+    public class Mangas
+    {
+        [Key]
+        [DataMember]
+        public int Id { get; set; }
+
+        
+        [DataMember]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = "";
+        
+        [DataMember]
+        public string Gender { get; set; } = "";
+
+        [DataMember]
+        public string Title { get; set; } = "";
+
+        [DataMember]
+        public string Author { get; set; } = "";
+
+        [DataMember]
+        public string Status { get; set; } = "";
+
+        [DataMember]
+        public string Synopsis { get; set; } = "";
+
+        [DataMember]
+        public string Ratings { get; set; } = "";
+
+        [DataMember]
+        public string Images { get; set; } = "";
+
+        [DataMember]
+        public string Language { get; set; } = "";
+
+        [DataMember]
+        public string CountryOfOrigin { get; set; } = "";
+
+        [DataMember]
+        public int YearOfPublication { get; set; } = 0;
+
+        [DataMember]
+        public string AlternateNames { get; set; } = ""; 
+
+
+    }
+}

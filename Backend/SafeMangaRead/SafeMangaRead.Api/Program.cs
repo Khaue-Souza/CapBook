3a48635a991ef5965d1b169325821dd14593d14e
Khaue-Souza <81392244+Khaue-Souza@users.noreply.github.com>
2023-11-23 15:14:48 -0300

    Testando tirar a senha do banco do codigo novamente

--- Backend/SafeMangaRead/SafeMangaRead.Api/Program.cs
+++ Backend/SafeMangaRead/SafeMangaRead.Api/Program.cs
@@ -1,9 +1,22 @@
 using MangaNovelsAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+DotNetEnv.Env.Load();
+
+builder.Services.AddDbContext<APIdbcontext>(options =>
+{
+    // Agora você pode acessar as variáveis de ambiente, incluindo a senha do banco de dados
+    string dbPassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+
+    var connectionString = $"Server=tcp:server-trcnf5ssqiqfc.database.windows.net,1433;Initial Catalog=db-trcnf5ssqiqfc;Persist Security Info=False;User ID=zmgxljeq;Password={dbPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    options.UseSqlServer(connectionString);
+});
 
 // Add services to the container.
 
@@ -24,9 +37,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddDbContext<APIdbcontext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
